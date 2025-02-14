b50af74114372d7aaa840dbb61e1a4a9792980af
luisa <luis@pulseb2b.com>
2025-02-14 12:50:55 -0800

    sequalize

--- backend/models.js
+++ backend/models.js
@@ -1,5 +1,5 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = require('./config/database');
+const sequelize = require('./config/database').sequelize;
 
 // Item Schema
 const Item = sequelize.define('Item', {
