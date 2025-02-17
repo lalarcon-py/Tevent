64c159c57c7d493014d4db6dd1c3b30550ae0201
luisa <luis@pulseb2b.com>
2025-02-17 10:11:54 -0800

    update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -9,7 +9,7 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    url: process.env.DATABASE_URL,
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
