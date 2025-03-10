21ac0dd612511ef023dfd8769367b97eb68269cb
Luis Alarcon <luisalarcongato@gmail.com>
2025-03-10 12:39:09 -0700

    hard code client id

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -262,12 +262,14 @@ const registerCommands = async () => {
       }
     ];
 
+    const CLIENT_ID = '1333905158496587816';
+
     // Register commands with Discord
     const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN);
     console.log('Started refreshing application (/) commands.');
     
     await rest.put(
-      Routes.applicationCommands(process.env.DISCORD_CLIENT_ID),
+      Routes.applicationCommands(CLIENT_ID),
       { body: commands },
     );
 
