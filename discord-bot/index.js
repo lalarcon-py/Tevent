7cfbad3a58f4e4e3b2d4a9dd0cf66010faf13230
Luis Alarcon <luisalarcongato@gmail.com>
2025-03-13 16:49:26 -0700

    update db config path

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -6,7 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
-const { sequelize } = require('../config/database');
+const { sequelize } = require('../backend/config/database');
 
 
 
