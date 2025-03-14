14cf92f2addcbd578b3fca33a616369e1022d758
Luis Alarcon <luisalarcongato@gmail.com>
2025-03-13 17:44:48 -0700

    add cron to imports

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -6,6 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
+const cron = require('node-cron');
 
 console.log('Environment Check:', {
   CLIENT_ID: process.env.DISCORD_CLIENT_ID || 'missing',
